c9f0b33b522da468cce1a9cb04897f911a0af509
snam4 <snam4@saic.edu>
2014-10-23 23:50:11 -0400

    W8 10/23 - OnTriggerEnter
    OnTriggerEnter / OnMouseDown / Destroy

---
+++ W8_1023ontriggerenter.js
@@ -0,0 +1,33 @@
+﻿#pragma strict
+//10-23-2014
+//OnTriggerEnter - bare minimun script
+
+
+//declare your variable
+var speed :float = 10.0;
+//declare a game object variable to make a space to assign a target obj
+var target : GameObject;
+
+function Start () {
+	//automatic assignment "myCube" to target (game object variable)
+	target = GameObject.Find ("myCube");
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter (other : Collider) {
+	// This rotation will not be very interesting because it will be executed only for a frame (the frame of entering)
+	//target.transform.Rotate(0, speed * Time.deltaTime, 0 );
+	//if you want to rotate the cube when the character is in the trigger look into "OnTriggerStay"
+	
+	//So will print just a message to make sure that it works fine
+	Debug.Log ("I sense you");
+	}
+
+//Review for Mouse Interaction! 
+function OnMouseDown(){
+	//and new function "Destroy"
+	Destroy (target);
+}
