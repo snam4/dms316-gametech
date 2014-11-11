9ff12ca058c16574f726d5fa4b9b0f41fb5767fd
snam4 <snam4@saic.edu>
2014-11-11 10:03:41 -0500

    week 11 - 11-11
    Projectile, animation trigger

---
+++ W11_walkingCharacter.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+   if (Input.GetAxis("Vertical") > 0.2)
+       animation.CrossFade ("walk");
+   else
+      animation.CrossFade ("idle");
+
+	if (Input.GetButton("Fire1"))
+      animation.CrossFade("shoot");
+}
